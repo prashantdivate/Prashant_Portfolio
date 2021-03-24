fc98a2103396558ed7cf56c23cdb793442995f62
prashantdivate <diwateprashant44@gmail.com>
2021-03-24 17:58:32 +0530

    Initial Project Data Push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from  './components/testimonial/testimonial';
+import ContactUs from './components/contactus/contactus';
+import Footer from './components/footer/footer';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+  "imagebaseurl":"http://localhost:3000/",
+  "name": "Prashant Divate",
+  "role": "Embedded Software Engineer"
+  }
+export default resumeData
